79dc71089c7e728ea02aa6c1df6171828b60e195
zenway33 <zenway33@gmail.com>
2016-08-24 15:41:03 +0800

    guoke ajax

---
+++ guokr_ajax_mongodb/guokr.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+from bs4 import BeautifulSoup
+import requests
+import json
+import pymongo
+
+url = 'http://www.guokr.com/scientific/'
+
+def dealData(url):
+    client = pymongo.MongoClient('localhost', 27017)
+    guoke = client['guoke']
+    guokeData = guoke['guokeData']
+    web_data = requests.get(url)
+    datas = json.loads(web_data.text)
+    print(datas.keys())
+    for data in datas['result']:
+        print(data)
+        guokeData.insert_one(data)
+
+def start():
+    urls = ['http://www.guokr.com/apis/minisite/article.json?retrieve_type=by_subject&limit=20&offset={}&_=1462252453410'.format(str(i)) for i in range(20, 100, 20)]
+    for url in urls:
+        dealData(url)
+
+start()
